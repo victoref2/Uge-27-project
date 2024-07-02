58402f9c6b36fb45d7eac754f12433057fabbacb
victoref <victoref3@gmail.com>
2024-07-02 13:38:46 +0200

    Commited Parts

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,6 +1,102 @@
-﻿namespace Uge_27_project.ViewModels;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
-public class MainViewModel : ViewModelBase
+namespace Uge_27_project.ViewModels
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string Greeting => "Lets do some math!";
+
+        private string _inputText;
+
+        public string InputText
+        {
+            get => _inputText;
+            set
+            {
+                _inputText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ButtonCommand { get; }
+        public ICommand CalculateCommand { get; }
+        public ICommand ClearCommand { get; }
+
+        public MainViewModel()
+        {
+            ButtonCommand = new RelayCommand(param => AddInput(param));
+            CalculateCommand = new RelayCommand(param => Calculate());
+            ClearCommand = new RelayCommand(param => Clear());
+        }
+
+        private void AddInput(object param)
+        {
+            if (param != null)
+            {
+                InputText += param.ToString();
+            }
+        }
+
+        private void Calculate()
+        {
+            try
+            {
+                // A simple parser for the input string
+                var result = EvaluateExpression(InputText);
+                InputText = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                InputText = "Error";
+            }
+        }
+
+        private void Clear()
+        {
+            InputText = string.Empty;
+        }
+        private double EvaluateExpression(string expression)
+        {
+            var dataTable = new System.Data.DataTable();
+            return Convert.ToDouble(dataTable.Compute(expression, ""));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+    }
 }
--- Views/MainView.axaml.cs
+++ Views/MainView.axaml.cs
@@ -1,4 +1,8 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Uge_27_project.ViewModels;
 
 namespace Uge_27_project.Views;
 
@@ -6,6 +10,77 @@ public partial class MainView : UserControl
 {
     public MainView()
     {
-        InitializeComponent();
+        this.InitializeComponent();
+        this.AddHandler(KeyDownEvent, OnKeyDown, RoutingStrategies.Tunnel);
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    private void OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (DataContext is MainViewModel viewModel)
+        {
+            string key = e.Key.ToString();
+
+            // Handle numeric keys
+            if (e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                viewModel.InputText += (e.Key - Key.D0).ToString();
+            }
+            // Handle numpad keys
+            else if (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9)
+            {
+                viewModel.InputText += (e.Key - Key.NumPad0).ToString();
+            }
+            // Handle operator keys
+            else if (key == "Add" || key == "OemPlus")
+            {
+                viewModel.InputText += "+";
+            }
+            else if (key == "Subtract" || key == "OemMinus")
+            {
+                viewModel.InputText += "-";
+            }
+            else if (key == "Multiply" || key == "OemAsterisk")
+            {
+                viewModel.InputText += "*";
+            }
+            else if (key == "Divide" || key == "OemQuestion")
+            {
+                viewModel.InputText += "/";
+            }
+            else if (key == "OemOpenBrackets" || key == "D9" && (e.KeyModifiers & KeyModifiers.Shift) != 0)
+            {
+                viewModel.InputText += "(";
+            }
+            else if (key == "OemCloseBrackets" || key == "D0" && (e.KeyModifiers & KeyModifiers.Shift) != 0)
+            {
+                viewModel.InputText += ")";
+            }
+            // Handle Enter key for calculation
+            else if (e.Key == Key.Enter)
+            {
+                viewModel.CalculateCommand.Execute(null);
+            }
+            // Handle Escape key for clearing input
+            else if (e.Key == Key.Escape)
+            {
+                viewModel.ClearCommand.Execute(null);
+            }
+            else
+            {
+                // Prevent other keys from affecting the TextBox
+                e.Handled = true;
+            }
+        }
+    }
+
+    private void OnTextBoxKeyDown(object sender, KeyEventArgs e)
+    {
+        OnKeyDown(sender, e);
     }
 }
+
